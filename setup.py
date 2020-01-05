5ccdf88b93e687678df77a5ee18ec88d5af85978
WardPearce <wpearce6@gmail.com>
2020-01-06 02:14:53 +1300

    Added setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='aiob2',
+    version='0.0.1',
+    description='Asynchronous backblaze b2 wrapper.',
+    author='WardPearce',
+    author_email='contact@districtnine.host',
+    license='Apache License 2.0',
+    packages=['aiob2'],
+    zip_safe=False
+)

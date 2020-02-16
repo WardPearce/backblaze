c0078c8e0afc49aed2ba35fa00c53a4775176570
WardPearce <wpearce6@gmail.com>
2020-02-17 01:04:43 +1300

    Update aiob2.py, routes.py, and 2 more files...

--- aiob2/aiob2.py
+++ aiob2/aiob2.py
@@ -1,29 +1,77 @@
-from utils import B2Base
 from upload import B2Upload
+from routes import ROUTES
 
-class b2(B2Base):
+import aiohttp
+import aiofiles
+import os
+import asyncio
+import base64
+
+class b2(object):
     """ B2 API Interface. """
 
-    def __init__(self):
-        B2Base.__init__(self)
+    def __init__(self, application_key_id, application_key):
+        self.ROUTES = ROUTES
 
-        self.upload = B2Upload()
+        self.loop = asyncio.get_event_loop()
+        self.session = aiohttp.ClientSession(loop=self.loop)
+        self.loop.run_until_complete(self.auth(application_key_id, application_key))
+
+        self.upload = B2Upload(obj=self)
+
+    def get_bucket(self, bucket_id):
+        return {"bucketId": bucket_id}
+
+    def get_file_id(self, file_id):
+        return {"fileId": file_id}
+
+    def part_number(self, number):
+        if number > 10000 and number < 1:
+            raise Exception("InvalidPartNumber")
+
+    async def read_file(self, file_pathway):
+        if os.path.isfile(file_pathway):
+            async with aiofiles.open(file_pathway, mode="r") as f:
+                contents = await f.read()
+                
+            return contents
+
+        raise Exception("CantReadFile")
+
+    async def post(self, url, **kwargs):
+        async with self.session.post(url, headers=self.authorization, **kwargs) as resp:
+            if resp.status == 200:
+                return await resp.json()
+            else:
+                return False
+
+    async def auth(self, application_key_id, application_key):
+        """ https://www.backblaze.com/b2/docs/b2_authorize_account.html """
+
+        encoded_bytes = base64.b64encode("{}:{}".format(application_key_id, application_key).encode("utf-8"))
+        basic_auth_string = "Basic {}".format(str(encoded_bytes, "utf-8"))
+
+        async with self.session.get(self.ROUTES["authorize"], headers={"Authorization": basic_auth_string}) as resp:
+            if resp.status == 200:
+                resp_json = await resp.json()
+
+                self.api_url = resp_json["apiUrl"]
+                self.download_url = resp_json["downloadUrl"]
+                self.account_id = resp_json["accountId"]
+                self.authorization = {"Authorization": resp_json["authorizationToken"]}
+
+                return resp_json
+            else:
+                raise Exception("InvalidAuthorization")
 
 if __name__ == "__main__":
-    import asyncio
-    import aiohttp
-
-    b2_client = b2()
+    b2_client = b2(application_key_id="", application_key="")
 
     async def testing():
-        aiohttp_session = b2_client.aiohttp_init(aiohttp.ClientSession(loop=loop))
+        print(await b2_client.upload.get_url(bucket_id="33e138c438fbe35e6be90b11"))
 
-        await b2_client.auth(application_key_id="", application_key="")
-
-        await b2_client.upload.part()
-
-        await aiohttp_session.close()
-
+        await b2_client.session.close()
+        
     loop = asyncio.get_event_loop()
     loop.run_until_complete(testing())
     loop.close()
--- aiob2/upload.py
+++ aiob2/upload.py
@@ -1,8 +1,28 @@
-from utils import B2Base
+class B2Upload(object):
+    def __init__(self, obj):
+        self.obj = obj
 
-class B2Upload(B2Base):
-    def __init__(self):
-        B2Base.__init__(self)
+    async def get_url(self, bucket_id):
+        """ https://www.backblaze.com/b2/docs/b2_get_upload_url.html """
 
-    async def part(self):
-        print("foobar")
+        return await self.obj.post(self.obj.ROUTES["get_upload_url"].format(self.obj.api_url), json=self.obj.get_bucket(bucket_id))
+
+    async def get_part_url(self, file_id):
+        """ https://www.backblaze.com/b2/docs/b2_get_upload_part_url.html """
+
+        return await self.obj.post(self.obj.ROUTES["get_upload_part_url"].format(self.obj.api_url), json=self.obj.get_file_id(file_id))
+
+    async def file(self, file_name, file_pathway):
+        """ https://www.backblaze.com/b2/docs/b2_upload_file.html 
+            
+                file_name, name to save it under on the bucket.
+                file_pathway, pathway to the file.
+        """
+
+        file_data = await self.obj.read_file(file_pathway)
+        file_name = file_name.encode("utf-8")
+
+    async def part(self, x_bz_part_number: int, content_lenght, x_bz_content_sha1, **kwargs):
+        """ https://www.backblaze.com/b2/docs/b2_upload_part.html """
+
+        self.obj.part_number(x_bz_part_number)

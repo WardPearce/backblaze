77a98562d355a8b75668d7d943ce9ce8e91ac161
WardPearce <wpearce6@gmail.com>
2020-02-25 18:51:55 +1300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from setuptools import setup
 
+def get_requirements():
+    with open("requirements.txt") as f:
+        return f.read().splitlines()
+
 setup(
     name='aiob2',
     version='0.0.5',
     description='Asynchronous backblaze b2 wrapper.',
     author='WardPearce',
     author_email='contact@districtnine.host',
+    install_requires=get_requirements(),
     license='Apache License 2.0',
     packages=['aiob2'],
     zip_safe=False

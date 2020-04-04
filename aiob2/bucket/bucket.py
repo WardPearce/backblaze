fe4335d24f405ab4e252069d78f52aa30c19bded
WardPearce <wpearce6@gmail.com>
2020-04-05 12:53:44 +1300

    Update bucket.py and delete.py

--- aiob2/bucket/bucket.py
+++ aiob2/bucket/bucket.py
@@ -1,6 +1,5 @@
 from .upload import Upload
 from .file import File
-from .delete import Delete
 
 class Bucket(object):
     def __init__(self, bucket_id, obj):
@@ -14,13 +13,13 @@ class Bucket(object):
         return Upload(bucket_id=self.bucket_id, obj=self.obj)
 
     @property
-    def delete(self):
-        """ Delete Object """
-
-        return Delete(bucket_id=self.bucket_id, obj=self.obj)
-
-    @property
     def file(self):
         """ File object """
 
         return File(bucket_id=self.bucket_id, obj=self.obj)
+
+    async def delete(self):
+        """ https://www.backblaze.com/b2/docs/b2_delete_bucket.html """
+
+        return await self.obj._post(url=self.obj.ROUTES["delete_bucket"].format(self.obj.api_url),
+                                    json={"accountId": self.obj.account_id, "bucketId": self.bucket_id,})

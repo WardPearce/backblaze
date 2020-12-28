3ac66789b00dba0f40925a7569d4307cd2667783
Ward <wardpearce@protonmail.com>
2020-12-28 16:04:16 +1300

    Changed doc theme

--- source/conf.py
+++ source/conf.py
@@ -59,7 +59,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'material'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -67,3 +67,7 @@ html_theme = 'alabaster'
 html_static_path = ['_static']
 
 master_doc = 'index'
+
+html_theme_options = {
+    'theme_primary': '#430e8e'
+}
